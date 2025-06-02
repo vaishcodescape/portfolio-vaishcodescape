0580be9a552a6f0728027a5035a056b4d2c5463c
Aditya Vaish <adityavaish846@gmail.com>
2025-06-01 17:57:12 -0700

    new_skill added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Vaish-Folio",
-  description: "Vaish-Folio",
+  description: "Personal Portfolio",
 };
 
 export default function RootLayout({ children }) {
