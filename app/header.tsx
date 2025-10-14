61f2e38a0bba4799378b0813e3543f21d582102b
Aditya Vaish <adityavaish846@gmail.com>
2025-10-14 19:09:06 +0530

    role changed

--- app/header.tsx
+++ app/header.tsx
@@ -26,7 +26,7 @@ export function Header() {
             className="text-zinc-600 dark:text-zinc-500"
             delay={0.5}
           >
-            Software Engineer
+            Software Developer
           </TextEffect>
         </div>
       </div>
